f20f219e25d8b67678cef9735e549dc74176eaa5
Santosh <santoshkumar23kky@gmail.com>
2023-09-08 07:44:45 +0530

    Day_10

---
+++ Day_10/dateAndTime.js
@@ -0,0 +1,23 @@
+let myDate = new Date();
+console.log(myDate);
+// console.log(myDate.toString());
+// console.log(typeof toString());
+// console.log(myDate.getDate());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getHours());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getTime());
+// console.log(myDate.getSeconds());
+// console.log(myDate.getMilliseconds());
+
+let myCreateDate = new Date(2023, 9 ,6)
+let myCreateDate1 = new Date("2023-01-14")
+// console.log(myCreateDate1.toLocaleString())
+// console.log(myCreateDate.toDateString())
+
+let myTimeStamp = Date.now;
+// console.log(myTimeStamp());
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+

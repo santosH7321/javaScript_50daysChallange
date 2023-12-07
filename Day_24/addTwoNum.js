1a96abe473a08bcca179b0c29b690f7a665bd51c
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-07 23:29:23 +0530

    Function

---
+++ Day_24/addTwoNum.js
@@ -0,0 +1,6 @@
+function sum(Num1, Num2){
+    return Num1 + Num2;
+}
+
+let result = sum(3, 4);
+console.log(result);
---
+++ Day_24/remainderOfTwo.js
@@ -0,0 +1,7 @@
+function rem(Num1, Num2){
+    let result = Num1 % Num2;
+    return result;
+}
+
+let answer = rem(5, 2);
+console.log(answer);

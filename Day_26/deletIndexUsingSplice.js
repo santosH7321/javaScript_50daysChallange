ed09ff5305da11b4d15b99f291add927725dad4c
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-10 23:50:36 +0530

    SliceAndSplice

---
+++ Day_26/deletIndexUsingSplice.js
@@ -0,0 +1,3 @@
+let number = [1,2,3,4,5];
+let deletedNumber = number.splice(3, number.length); //[4, 5]
+console.log(deletedNumber);
---
+++ Day_26/reducingArrayUsingSlice.js
@@ -0,0 +1,3 @@
+let marks = [10,20,30,40,50,60,70,80,90];
+let combineArray = marks.slice(2,6);
+console.log(combineArray);

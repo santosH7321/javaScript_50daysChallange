b65a8b0aba985acc5a60eacd5cfc26254fef66bc
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-10 23:36:43 +0530

    EmpatyArray

---
+++ Day_26/dateTIme.js
@@ -0,0 +1,11 @@
+let date = new Date();
+console.log(date); // 2023-12-10T17:54:48.633Z
+
+
+let date2 = new Date('Oct 9 2002 07:15');
+
+console.log(date2); //2002-10-09T01:45:00.000Z
+
+let date3 = new Date(2002, 10, 9, 7);
+console.log(date3);
+
---
+++ Day_26/emptyArray.js
@@ -0,0 +1,8 @@
+let number = [1,2,3,4,5];
+// let number2 = number;
+number = [];
+// number.len = 0;
+console.log(number.length);
+// console.log(number2.length);
+number.splice(0, number.length);
+console.log(number);

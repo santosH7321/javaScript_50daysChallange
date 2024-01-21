264b208b095480a4222533a2d81da3c425130f26
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-21 23:57:14 +0530

    Day_30

---
+++ Day_30/switch.js
@@ -0,0 +1,13 @@
+let n = 2;
+switch(n){
+    case 1: 
+        console.log("case 1");
+        break;
+    case 2:
+        console.log("Case 2");
+        break;
+    case 3:
+        console.log("Case 3");
+        break;
+    
+}

3782b8af0eba1f25ffac422e5da63fb7a2d233a1
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-07-13 22:34:13 +0530

    chaiorcode day 1

---
+++ ChaiOrCodeDay1/variable.js
@@ -0,0 +1,15 @@
+// Task 1
+var a = 1;
+console.log(a);
+
+// Task 2
+let b = "Santosh";
+console.log(b);
+
+// Task 3
+const c = false;
+console.log(c);
+
+// Task 4
+var d = [1, "Santosh", true,  {}, [1,2,3]];
+console.log(typeof(d));
